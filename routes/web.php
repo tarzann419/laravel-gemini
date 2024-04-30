96eb7689386fc1ac9f680304c042f95d9fd5fed4
dantheman <danogbo0@gmail.com>
2024-04-30 15:13:45 +0100

    gemini-image-text-input

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use Gemini\Data\Blob;
+use Gemini\Enums\MimeType;
 use Illuminate\Support\Facades\Route;
 use Gemini\Laravel\Facades\Gemini;
-
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +20,26 @@ Route::get('/gemini', function () {
     }
     // dd($gemini->text());
 }); 
+
+
+Route::get('/gemini-image-text-input', function(Request $request){
+    // dd($request->message);
+    $result = Gemini::geminiProVision()
+    ->generateContent([
+        $request->content,
+        new Blob(
+            mimeType: MimeType::IMAGE_JPEG,
+            data: base64_encode(
+                file_get_contents($request->image)
+            )
+        )
+    ]);
+
+    // you can pass this in your postman
+    // {
+    // "image": "https://storage.googleapis.com/generativeai-downloads/images/scones.jpg",
+    // "content": "What is this picture?"
+    // }   
+
+    echo $result->text();
+});
